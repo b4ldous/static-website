02ac37dc736916132415c0ccf4e22f5dd1942bd0
b4ldous <baldous@protonmail.com>
2022-11-16 20:51:30 -0600

    scroll button was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,75 @@
-
+import React from 'react'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import WelcomePage from "./WelcomePage";
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
 
 
-function App() {
+function App(props) {
 
 
   return (
     <>
-    <div className="App">
-        <h1 align="center">Navigation</h1>
-        <h2 align="center">
+
+<CssBaseline />
+      <AppBar>
+        <Toolbar>
+          
+          <h2 align="center">
           <AnchorLink href="#header">
             <button>Header</button>
           </AnchorLink>
@@ -21,9 +80,37 @@ function App() {
             <button>Footer</button>
           </AnchorLink>
         </h2>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <Container>
+      
         <WelcomePage/>
-      </div>
-    
+      </Container>
+      <ScrollTop {...props}>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
      
     </>
   )
